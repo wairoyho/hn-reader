4b3d22cc4ff40109c52b0be115f91e1efa167a40
wairoyho <wairoyho@users.noreply.github.com>
2021-05-04 22:32:12 +0800

    chore: init commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const IndexPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Home | Next.js + TypeScript Example</title>
+      </Head>
+      <h1>Hello Next.js 👋</h1>
+    </div>
+  );
+};
+
+export default IndexPage;
