8bbfcaf75451cab5589fda240957f140a12331be
wairoyho <wairoyho@users.noreply.github.com>
2021-05-05 23:25:33 +0800

    fix: disable tailwindcss jit mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  mode: "jit",
+  // mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
@@ -11,9 +11,6 @@ module.exports = {
         orange: colors.orange,
       },
     },
-  },
-  corePlugins: {
-    flexWrap: true,
   },
   variants: {
     extend: {},
