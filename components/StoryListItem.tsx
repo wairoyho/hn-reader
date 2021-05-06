33b4f909c8162b5969f2fac262dd29fd9150a075
wairoyho <wairoyho@users.noreply.github.com>
2021-05-07 02:16:41 +0800

    refactor: StoryListItem style

--- components/StoryListItem.tsx
+++ components/StoryListItem.tsx
@@ -8,6 +8,14 @@ interface StoryListItemProps {
   storyId: number;
 }
 
+const getColorCode = (score: number) => {
+  if (score < 100) return "#FDBA74";
+  if (score < 300) return "#FB923C";
+  if (score < 500) return "#F97316";
+  if (score < 1000) return "#EA580C";
+  if (score < 2000) return "#C2410C";
+};
+
 const StoryListItem = (props: StoryListItemProps) => {
   const { storyId } = props;
 
@@ -23,24 +31,51 @@ const StoryListItem = (props: StoryListItemProps) => {
   }, [storyId]);
 
   return (
-    <li className="relative box-border w-full flex justify-start items-center text-left no-underline py-2 px-4 shadow-sm">
+    <li className="relative box-border w-full flex justify-start items-center text-left no-underline py-3 px-4 shadow-sm">
       {story ? (
-        <div>
-          <div>
+        <div className="w-full">
+          <div className="flex flex-wrap prose-sm text-gray-700">
+            {/* <span className="mr-1">{`${story.score} points`}</span> */}
+            <span className="">{`@${story.by}`}</span>
+            {/* <span className="ml-1">{" | "}</span> */}
+            <span className="ml-2">{`${getRelativeTime(
+              new Date(story.time * 1000)
+            )}`}</span>
+            <div className="flex ml-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 0 24 24"
+                width="24px"
+                fill="#000000"
+              >
+                <path d="M0 0h24v24H0V0z" fill="none" />
+                <path d="M7 14l5-5 5 5H7z" fill={getColorCode(story.score)} />
+              </svg>
+              <span>{`${story.score}`}</span>
+            </div>
+            {/* <span className="ml-2">💛 {`${story.score}`}</span> */}
+            {/* <span className="ml-1">{" | "}</span> */}
+            <div className="flex ml-auto">
+              <svg
+                className="m-auto"
+                xmlns="http://www.w3.org/2000/svg"
+                height="18px"
+                viewBox="0 0 24 24"
+                width="18px"
+                fill="#000000"
+              >
+                <path d="M0 0h24v24H0V0z" fill="none" />
+                <path d="M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H6l-2 2V4h16v12z" />
+              </svg>
+              <span className="ml-1">{`${(story?.kids ?? []).length}`}</span>
+            </div>
+          </div>
+          <div className="mt-2">
             <p className="prose-sm text-gray-500">
               <span className="inline prose-lg text-black">{story.title}</span>
               {story.url && <> ({new URL(story.url).hostname})</>}
             </p>
-          </div>
-          <div className="flex flex-wrap prose-sm text-gray-700">
-            <span className="mr-1">{`${story.score} points`}</span>
-            <span className="mr-1">{`by ${story.by}`}</span>
-            <span className="mr-1">{" | "}</span>
-            <span className="mr-1">{`${getRelativeTime(
-              new Date(story.time * 1000)
-            )}`}</span>
-            <span className="mr-1">{" | "}</span>
-            <span>{`${(story?.kids ?? []).length} comments`}</span>
           </div>
         </div>
       ) : (
